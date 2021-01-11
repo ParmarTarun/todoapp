c55463843969dd73773c3ac7f6ed6d72cfaa0992
tarunparmar <parmartarun.dev@gmail.com>
2021-01-11 17:19:38 +0530

    :zap: functioning of tabs

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,1 +1,15 @@
 // put js for todo app here
+const toggleTab = (event) => {
+    //for tabs
+    const tabs = event.currentTarget.parentNode.children
+    for (let x of tabs)
+        x.classList.remove("selected")
+    event.currentTarget.classList.add("selected")
+
+    //forcontent
+    const contentToShow = event.currentTarget.id
+    const tabContents = document.querySelector(".tabs-content").children;
+    for (let x of tabContents)
+        x.classList.remove("active")
+    document.querySelector("."+contentToShow).classList.add("active")
+}
